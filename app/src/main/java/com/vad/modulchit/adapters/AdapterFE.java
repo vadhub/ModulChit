30b0127a829f25ea7019623a4e68ea7e8e9c145e
Vadim <gabderahmanov99@gmail.com>
2021-06-02 23:39:43 +0300

    adapter complite

--- app/src/main/java/com/vad/modulchit/adapters/AdapterFE.java
+++ app/src/main/java/com/vad/modulchit/adapters/AdapterFE.java
@@ -1,11 +1,14 @@
 package com.vad.modulchit.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.vad.modulchit.R;
 import com.vad.modulchit.pojos.TableNumberFE;
 
 import java.util.List;
@@ -26,22 +29,41 @@ public class AdapterFE extends RecyclerView.Adapter<AdapterFE.MyViewHolderFE> {
     @NonNull
     @Override
     public MyViewHolderFE onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.table_counter_fe, parent, false);
+        return new MyViewHolderFE(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull AdapterFE.MyViewHolderFE holder, int position) {
-
+        holder.textViewA.setText(tableNumberFES.get(position).getAfe()+"");
+        holder.textViewM.setText(tableNumberFES.get(position).getM()+"");
+        holder.textViewN.setText(tableNumberFES.get(position).getN()+"");
+        holder.textViewP.setText(tableNumberFES.get(position).getP()+"");
+        holder.textViewR.setText(tableNumberFES.get(position).getRfe()+"");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return tableNumberFES.size();
     }
 
     public class MyViewHolderFE extends RecyclerView.ViewHolder {
+
+        private TextView textViewA;
+        private TextView textViewM;
+        private TextView textViewN;
+        private TextView textViewP;
+        private TextView textViewR;
+
+
         public MyViewHolderFE(@NonNull View itemView) {
             super(itemView);
+
+            textViewA = (TextView) itemView.findViewById(R.id.textViewAfe);
+            textViewM = (TextView) itemView.findViewById(R.id.textViewMfe);
+            textViewN = (TextView) itemView.findViewById(R.id.textViewNfe);
+            textViewP = (TextView) itemView.findViewById(R.id.textViewPfe);
+            textViewR = (TextView) itemView.findViewById(R.id.textViewRfe);
         }
     }
 }
