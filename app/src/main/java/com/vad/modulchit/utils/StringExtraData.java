701087e64a3e5d62615138f8048f46cdba21b454
Vadim <gabderahmanov99@gmail.com>
2022-01-14 13:53:37 +0300

    add calculating q and r

--- app/src/main/java/com/vad/modulchit/utils/StringExtraData.java
+++ app/src/main/java/com/vad/modulchit/utils/StringExtraData.java
@@ -2,8 +2,14 @@ package com.vad.modulchit.utils;
 
 public class StringExtraData {
     public String extraGCDE(int a, int b, int q, int r, int x1, int x2, int y1, int y2){
-        String result ="q = "+a+"/"+b+"= "+q+";\n"+
-                "r = "+a+" - "+b+"*"+q+" = "+ r+";\n" //r = 56 - 11*5 = 1;
+
+        q = a/b;
+        r = a - (b*q);
+
+        String result =
+                "a = "+ a +"(b pre);\n"+
+                "q = "+a+"/"+b+"= "+q+";\n"+
+                "r = "+a+" - "+b+"*"+q+" = "+r+";\n" //r = 56 - 11*5 = 1;
                         +"x = ("+x1+","+x2+");\n" //x = (1,0);
                         +"y = ("+x1+","+x2+") - "+ q +"*("+y1+","+y2+") = ("+(x1-(q*y1))+","+(x2-(q*y2))+");\n"; // y = (1,0) - 1*(0,1)
 
