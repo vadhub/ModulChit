bf3d260af7eb89503387916756821f6fc588d89d
Vadim <gabderahmanov99@gmail.com>
2022-01-16 12:50:56 +0300

    add encrypt explanation and alphabet explanation

---
+++ app/src/main/java/com/vad/modulchit/screens/rsa/crypt/explcrypt/FragmentCryptExpl.java
@@ -0,0 +1,21 @@
+package com.vad.modulchit.screens.rsa.crypt.explcrypt;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vad.modulchit.R;
+
+public class FragmentCryptExpl extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_crypt_expl, container, false);
+    }
+}
