fe752696017726f9b084e3a8c719b28fbfc4701e
Vadim <gabderahmanov99@gmail.com>
2022-01-16 13:33:43 +0300

    create decrypt explanation

---
+++ app/src/main/java/com/vad/modulchit/screens/rsa/decrypt/expldecrypt/FragmentDecryptExpl.java
@@ -0,0 +1,22 @@
+package com.vad.modulchit.screens.rsa.decrypt.expldecrypt;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vad.modulchit.R;
+
+public class FragmentDecryptExpl extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_decrypt_expl, container, false);
+    }
+}
