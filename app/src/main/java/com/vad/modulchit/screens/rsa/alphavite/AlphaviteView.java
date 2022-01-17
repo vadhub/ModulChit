9f93d7005f3327f32bf63eaa3d948775709832aa
Vadim <gabderahmanov99@gmail.com>
2022-01-17 13:59:47 +0300

    delete fragmentLoad

--- app/src/main/java/com/vad/modulchit/screens/rsa/alphavite/AlphaviteView.java
+++ app/src/main/java/com/vad/modulchit/screens/rsa/alphavite/AlphaviteView.java
@@ -1,13 +1,9 @@
 package com.vad.modulchit.screens.rsa.alphavite;
 
-import androidx.fragment.app.Fragment;
-
 import com.vad.modulchit.screens.supportable.Supportable;
 
 import java.util.List;
 
 public interface AlphaviteView extends Supportable {
-
     void alphaviteLoad(List<Integer> alphaviteCodes);
-    void fragmentLoad(Fragment fragment);
 }
