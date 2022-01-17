e2ea53316f362236f67284a987282ee17add84d7
Vadim <gabderahmanov99@gmail.com>
2022-01-17 13:57:41 +0300

    move Custom action to package

--- app/src/main/java/com/vad/modulchit/screens/contract/HasCustomAction.java
+++ app/src/main/java/com/vad/modulchit/screens/contract/HasCustomAction.java
@@ -1,10 +1,5 @@
 package com.vad.modulchit.screens.contract;
 
 public interface HasCustomAction {
-    void setCustomAction();
-}
-
-class CustomAction {
-    int iconRes;
-    Runnable onCustomAction;
+    CustomActionFragment setCustomAction(Navigator navigator);
 }
