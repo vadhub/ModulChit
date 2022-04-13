10e6f961733b80cd5d993989a1049f61b859a0d5
vadhub <gabderahmanov99@gmail.com>
2022-04-13 12:52:08 +0300

    create test on buublesort

--- app/src/test/java/com/vad/modulchit/ExampleUnitTest.java
+++ app/src/test/java/com/vad/modulchit/ExampleUnitTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.vad.modulchit.utils.sort.BubbleSort;
+
+import java.util.Arrays;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +17,12 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test public void bubblesort_isCorrect() {
+        BubbleSort b = new BubbleSort();
+        int[] arr = {3, 5, 4, 1, 2};
+        int[] arrSort = b.sort(arr);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, arrSort);
     }
 }
