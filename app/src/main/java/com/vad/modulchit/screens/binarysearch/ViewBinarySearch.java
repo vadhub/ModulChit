a1e524847d50de6978a443b9ce8eca723b59b282
vadhub <gabderahmanov99@gmail.com>
2022-04-13 18:00:22 +0300

    add for binary search elements

---
+++ app/src/main/java/com/vad/modulchit/screens/binarysearch/ViewBinarySearch.java
@@ -0,0 +1,15 @@
+package com.vad.modulchit.screens.binarysearch;
+
+import android.content.Context;
+import android.view.View;
+
+public class ViewBinarySearch extends View {
+
+    public ViewBinarySearch(Context context) {
+        super(context);
+    }
+
+
+
+
+}
--- app/src/main/java/com/vad/modulchit/utils/search/BinarySearchImpl.java
+++ app/src/main/java/com/vad/modulchit/utils/search/BinarySearchImpl.java
@@ -6,18 +6,26 @@ public class BinarySearchImpl implements BinarySearch{
         int lowIndex = 0;
         int highIndex = arr.length-1;
 
+        System.out.println("low " + highIndex);
+        System.out.println("high " + highIndex);
+
         int elementPos = -1;
 
         while (lowIndex <= highIndex) {
             int midIndex = (lowIndex + highIndex) / 2;
+            System.out.println("mid " + midIndex);
             if (element == arr[midIndex]) {
                 elementPos = midIndex;
                 break;
             } else if (element < arr[midIndex]) {
                 highIndex = midIndex-1;
+                System.out.println("high " + highIndex);
             } else if (element > arr[midIndex]) {
                 lowIndex = midIndex+1;
+                System.out.println("low " + lowIndex);
             }
+
+            System.out.println();
         }
         return elementPos;
     }
