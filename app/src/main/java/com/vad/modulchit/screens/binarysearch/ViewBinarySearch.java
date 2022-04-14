11ea9e621b4f78eeb4ecd2381c2ac0b8132bc18b
vadhub <gabderahmanov99@gmail.com>
2022-04-14 13:53:12 +0300

    correct coordinate & draw triangle

--- app/src/main/java/com/vad/modulchit/screens/binarysearch/ViewBinarySearch.java
+++ app/src/main/java/com/vad/modulchit/screens/binarysearch/ViewBinarySearch.java
@@ -21,16 +21,35 @@ public class ViewBinarySearch extends View {
         for (int i = 200; i < 740; i+=100) {
             paint.setColor(Color.BLACK);
             paint.setStrokeWidth(5);
-            canvas.drawRect(i, 100, i-110, 230, paint);
+            canvas.drawRect(i, 100, i-100, 230, paint);
 
             paint.setColor(Color.GREEN);
             paint.setStrokeWidth(5);
-            canvas.drawRect(i-10, 110, i-100,220, paint);
+            canvas.drawRect(i-10, 110, i-90,220, paint);
         }
 
         paint.setStrokeWidth(5);
         paint.setColor(Color.BLACK);
 
-        canvas.drawLine(396,200, 390, 396, paint);
+        canvas.drawLine(400,233, 400, 380, paint);
+
+        paint.setStyle(Paint.Style.STROKE);
+
+        paint.setColor(Color.BLACK);
+        drawTriangle(canvas, paint, 400, 395, 30);
+
+    }
+
+    public void drawTriangle(Canvas canvas, Paint paint, int x, int y, int width) {
+        int halfWidth = width / 2;
+
+        Path path = new Path();
+        path.moveTo(x, y + halfWidth); // Top
+        path.lineTo(x + halfWidth, y - halfWidth); // Bottom left
+        path.lineTo(x - halfWidth, y - halfWidth); // Bottom right
+        //path.lineTo(x, y + halfWidth); // Back to Top
+        path.close();
+
+        canvas.drawPath(path, paint);
     }
 }
