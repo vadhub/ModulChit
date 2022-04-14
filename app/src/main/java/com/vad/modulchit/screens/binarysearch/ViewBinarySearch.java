b070bd0617d17d09556b39782c4130c69efe70bb
vadhub <gabderahmanov99@gmail.com>
2022-04-14 13:27:22 +0300

    draw line

--- app/src/main/java/com/vad/modulchit/screens/binarysearch/ViewBinarySearch.java
+++ app/src/main/java/com/vad/modulchit/screens/binarysearch/ViewBinarySearch.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 
 public class ViewBinarySearch extends View {
@@ -18,7 +19,6 @@ public class ViewBinarySearch extends View {
     protected void onDraw(Canvas canvas) {
 
         for (int i = 200; i < 740; i+=100) {
-
             paint.setColor(Color.BLACK);
             paint.setStrokeWidth(5);
             canvas.drawRect(i, 100, i-110, 230, paint);
@@ -27,5 +27,10 @@ public class ViewBinarySearch extends View {
             paint.setStrokeWidth(5);
             canvas.drawRect(i-10, 110, i-100,220, paint);
         }
+
+        paint.setStrokeWidth(5);
+        paint.setColor(Color.BLACK);
+
+        canvas.drawLine(396,200, 390, 396, paint);
     }
 }
