f997e33c43711f7431f0838675628b09ce8686f7
vadhub <gabderahmanov99@gmail.com>
2022-04-23 14:53:37 +0300

    add edittext button, on click calculation binary search

--- app/src/main/java/com/vad/modulchit/screens/binarysearch/FragmentBinarySearch.java
+++ app/src/main/java/com/vad/modulchit/screens/binarysearch/FragmentBinarySearch.java
@@ -8,26 +8,38 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.vad.modulchit.R;
 import com.vad.modulchit.screens.contract.HasCustomTitle;
 
 public class FragmentBinarySearch extends Fragment implements HasCustomTitle {
 
+    private EditText editTextArray;
+    private CustomViewBinarySearch customViewBinarySearch;
+    private Button btn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_binary_search, container, false);
+        editTextArray = (EditText) v.findViewById(R.id.editTextEnterArray);
+        customViewBinarySearch = v.findViewById(R.id.binary_search);
+        btn = v.findViewById(R.id.btnBinarySearch);
+        btn.setOnClickListener(v1 -> customViewBinarySearch.setArr(parseStringToArray(editTextArray.getText().toString())));
+        return v;
+    }
 
+    public static int[] parseStringToArray(String str) {
+        String[] spitString = str.split(",");
+        int[] tempArr = new int[spitString.length];
 
-        try {
-            View v = inflater.inflate(R.layout.fragment_binary_search, container, false);
-//            CustomViewBinarySearch customViewBinarySearch = (CustomViewBinarySearch) v.findViewById(R.id.binary_search);
-            return v;
-        } catch (Exception e) {
-//            Log.i("custom view", R.id.binary_search+"");
-            Log.e("error inflate", e.getMessage());
-            throw e;
+        for (int i = 0; i < spitString.length; i++) {
+            tempArr[i] = Integer.parseInt(spitString[i]);
         }
+
+        return tempArr;
     }
 
     @Override
