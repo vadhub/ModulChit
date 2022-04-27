7906a9719854e5dcdfb2b3ab440280f1d0edcac1
vadhub <gabderahmanov99@gmail.com>
2022-04-27 18:42:20 +0300

    create adapter for main screen

---
+++ app/src/main/java/com/vad/modulchit/adapters/AdapterMenu.java
@@ -0,0 +1,56 @@
+package com.vad.modulchit.adapters;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vad.modulchit.R;
+
+import java.util.List;
+
+public class AdapterMenu extends RecyclerView.Adapter<AdapterMenu.MyViewHolder> {
+
+    private List<String> namesMenu;
+
+    public List<String> getNamesMenu() {
+        return namesMenu;
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void setNamesMenu(List<String> namesMenu) {
+        this.namesMenu = namesMenu;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return getNamesMenu().size();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_menu, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.menuName.setText(namesMenu.get(position));
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView menuName;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            menuName = itemView.findViewById(R.id.text_view_name_screen);
+        }
+    }
+}
