b15d6eedf1ffd5f8db357bd907a8f2fade761fd2
vadhub <gabderahmanov99@gmail.com>
2022-05-12 22:22:53 +0300

    create tests

--- app/src/test/java/com/vad/modulchit/AlgebraModTest.java
+++ app/src/test/java/com/vad/modulchit/AlgebraModTest.java
@@ -2,15 +2,41 @@ package com.vad.modulchit;
 
 import static org.junit.Assert.assertEquals;
 
+import com.vad.modulchit.pojos.TableNumberGCDe;
 import com.vad.modulchit.utils.AlgebraMod;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 public class AlgebraModTest {
     int[] arr = {1, 2, 3, 0, -1, 4};
+    List<TableNumberGCDe> gcdeList;
+
+    @Before
+    public void setUp() {
+        AlgebraMod a = new AlgebraMod();
+        gcdeList = a.gcdGraph(180, 150);
+    }
 
     @Test
     public void searchMaxElement_isCorrect() {
         assertEquals(4, AlgebraMod.max(arr));
+    }
+
+    @Test
+    public void gcd_isCorrect() {
+        assertEquals(30, AlgebraMod.gcd(180, 150));
+    }
+
+    @Test
+    public void gcdeCoefY2_isCorrect() {
+        assertEquals(-1, gcdeList.get(gcdeList.size()-1).getY2());
+    }
+
+    @Test
+    public void gcdeCoefY1_isCorrect() {
+        assertEquals(1, gcdeList.get(gcdeList.size()-1).getY1());
     }
 }
