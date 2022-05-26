610bae5dfa2194ef955568cce073bfa7d93f3f18
vadhub <gabderahmanov99@gmail.com>
2022-05-26 23:43:46 +0300

    create unit test

--- app/src/test/java/com/vad/modulchit/QuickSortUnitTest.java
+++ app/src/test/java/com/vad/modulchit/QuickSortUnitTest.java
@@ -1,22 +1,20 @@
 package com.vad.modulchit;
 
-import com.vad.modulchit.animation.StepRecorder;
-import com.vad.modulchit.models.sort.Sort;
-import com.vad.modulchit.models.sort.shellimpl.ShellSort;
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class ShellSortUnitTest {
+import com.vad.modulchit.animation.StepRecorder;
+import com.vad.modulchit.models.sort.Sort;
+import com.vad.modulchit.models.sort.quickimpl.QuickSort;
+
+public class QuickSortUnitTest {
     int[] arr = {10, 4, 1, 8, 5, 4, 11, 9, 2, 1, 9, 3};
     int[] arrSorted = {1, 1, 2, 3, 4, 4, 5, 8, 9, 9, 10, 11};
 
     @Test
-    public void shellSort_isCorrect() {
-        Sort sort = new ShellSort();
+    public void sort_isCorrect() {
+        Sort sort = new QuickSort();
         StepRecorder stepRecorder = sort.sorting(arr);
-
         assertArrayEquals(stepRecorder.getSteps().get(stepRecorder.getSteps().size()-1), arrSorted);
-
     }
 }
