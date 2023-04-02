c00fe3a28449c8b8d59191cefe0623abf3446015
vadim <gabderahmanov99@gmail.com>
2023-04-02 21:18:58 +0300

    limit on view elements

--- app/src/main/java/com/vad/modulchit/screens/mg/ListMGpresenter.java
+++ app/src/main/java/com/vad/modulchit/screens/mg/ListMGpresenter.java
@@ -1,13 +1,24 @@
 package com.vad.modulchit.screens.mg;
 
 
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.util.Pair;
+
 import com.vad.modulchit.R;
 import com.vad.modulchit.models.AlgebraMod;
+import com.vad.modulchit.models.pojos.TableNumberNOK;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Predicate;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class ListMGpresenter {
@@ -22,36 +33,48 @@ public class ListMGpresenter {
         compositeDisposable = new CompositeDisposable();
     }
 
-    public void loadListMG(int m){
-        Disposable disposable = Observable.just(algebraMod)
-                .subscribeOn(Schedulers.io())
+    public void loadListMG(int m) {
+
+        Disposable disposable = Observable
+                .fromCallable(() -> {
+                    Log.d("##li", Thread.currentThread().getName());
+                    return algebraMod.getResult(algebraMod.nokGraph(m));
+                }).map(pair -> {
+                    List<TableNumberNOK> tableNumberNOKS;
+                    if (pair.first.size() > 100) {
+                        tableNumberNOKS = new ArrayList(pair.first.subList(0, 100));
+                        return new Pair<>(tableNumberNOKS, pair.second);
+                    }
+                    return pair;
+                })
+                .subscribeOn(Schedulers.single())
+                .doOnError(error -> System.out.println(error.getMessage() + " -----------------------"))
                 .observeOn(AndroidSchedulers.mainThread())
-                .map(o -> o.nokGraph(m))
-                .map(algebraMod::getResult)
-                .doOnError(error -> System.out.println(error.getMessage()+" -----------------------"))
-                .subscribe(pair -> listMGView.showData(pair.first, pair.second));
+                .subscribe(pair -> {
+                    Log.d("##li", Thread.currentThread().getName());
+                    listMGView.showData(pair.first, pair.second);
+                });
         compositeDisposable.add(disposable);
     }
 
 
+    public void showResult(String modStr) {
 
-    public void showResult(String modStr){
-
-        if(!modStr.equals("")){
+        if (!modStr.equals("")) {
             int m = -1;
 
-            try{
+            try {
                 m = Integer.parseInt(modStr);
-            }catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 listMGView.showError(R.string.warning_out_bounds);
             }
             listMGView.showTitle();
-            if(m!=0){
+            if (m != 0) {
                 loadListMG(m);
-            }else{
+            } else {
                 listMGView.showError(R.string.warning_zero);
             }
-        }else {
+        } else {
             listMGView.showError(R.string.warning_enter_text);
         }
     }
