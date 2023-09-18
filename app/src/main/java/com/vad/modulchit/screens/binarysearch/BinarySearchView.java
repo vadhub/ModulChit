9955701c49377115d73096f824d11c7d114e64fa
vadim <gabderahmanov99@gmail.com>
2023-09-18 20:20:06 +0300

    binary search view update

--- app/src/main/java/com/vad/modulchit/screens/binarysearch/BinarySearchView.java
+++ app/src/main/java/com/vad/modulchit/screens/binarysearch/BinarySearchView.java
@@ -10,17 +10,16 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.vad.modulchit.R;
 import com.vad.modulchit.models.pojos.BinarySearchModel;
-
-import java.util.Arrays;
 import java.util.List;
 
 public class BinarySearchView {
 
     private final Context context;
     private final LinearLayout linearRoot;
+    private LayoutInflater inflater;
+    private LinearLayout linearLayout;
 
     public BinarySearchView(Context context, LinearLayout linearRoot) {
         this.context = context;
@@ -31,17 +30,15 @@ public class BinarySearchView {
         FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.gravity = Gravity.CENTER_HORIZONTAL;
 
-        LayoutInflater inflater = LayoutInflater.from(context);
-
-        LinearLayout linearLayout;
-
-        int[] arr = searchModels.get(0).getArr();
+        inflater = LayoutInflater.from(context);
 
         for (BinarySearchModel b : searchModels) {
             Log.d("##bb", b.toString());
         }
 
         for (int k = 0; k < searchModels.size(); k++) {
+            BinarySearchModel binarySearchModel = searchModels.get(k);
+
             linearLayout = new LinearLayout(context);
             linearLayout.setGravity(Gravity.CENTER);
             linearLayout.setOrientation(LinearLayout.HORIZONTAL);
@@ -51,19 +48,38 @@ public class BinarySearchView {
             arrow.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
                     FrameLayout.LayoutParams.WRAP_CONTENT));
 
-            for (int i = searchModels.get(k).getLow(); i < searchModels.get(k).getHigh(); i++) {
-                View view = inflater.inflate(R.layout.item_element, linearRoot, false);
-                TextView index = view.findViewById(R.id.index);
-                TextView elementText = view.findViewById(R.id.element);
-
-                index.setText(i + "");
-                elementText.setText(arr[i] + "");
-
-                linearLayout.addView(view);
-            }
-            linearRoot.addView(linearLayout);
+            drawArray(binarySearchModel.getArr(), binarySearchModel.getLow(), binarySearchModel.getMid());
 
             if (k != searchModels.size()-1) linearRoot.addView(arrow);
         }
+    }
+
+    private void drawArray(int[] arr, int low, int mid) {
+        if (arr.length == 1) {
+            drawItem(arr, mid);
+            linearRoot.addView(linearLayout);
+            return;
+        }
+        for (int j : arr) {
+            View view = inflater.inflate(R.layout.item_element, linearRoot, false);
+            TextView index = view.findViewById(R.id.index);
+            TextView elementText = view.findViewById(R.id.element);
+            index.setText(low + "");
+            elementText.setText(j + "");
+            Log.d("i", low+"");
+            low++;
+            linearLayout.addView(view);
+        }
+
+        linearRoot.addView(linearLayout);
+    }
+
+    private void drawItem(int[] arr, int index) {
+        View view = inflater.inflate(R.layout.item_element, linearRoot, false);
+        TextView indexText = view.findViewById(R.id.index);
+        TextView elementText = view.findViewById(R.id.element);
+        indexText.setText(index + "");
+        elementText.setText(arr[0] + "");
+        linearLayout.addView(view);
     }
 }
